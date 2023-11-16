d3e148c28a5cb1f7ab4cfa6fc4078f74840287a0
Josh Kersker <jfk17a@acu.edu>
2023-11-16 10:06:37 -0600

    Finished modules 5 and 6, I have completed this course. Keeping matterials for reference.

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,7 +8,13 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
+    delete_list = []
+    for key, value in user_pref.items():
+        if value == None or value == "":
+            delete_list.append(key)
+    for i in range(0, len(delete_list)):
+        del user_pref[delete_list[i]]
+    return user_pref
 
 
 print(update_preferences(user_preferences))
--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,5 +1,9 @@
 def has_unique_characters(data):
-    return False
+    return len(set(data)) == len(data)
+    # set_data = set(data)
+    # if len(set_data) == len(data):
+    #     return True
+    # return False
 
 print(has_unique_characters('sample'))
 print(has_unique_characters('hello world'))
